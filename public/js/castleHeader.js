3144318273103a053c7c613c8e25174088d13a8f
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-12-05 09:27:17 +0000

    Full stars added with randomness

--- public/js/castleHeader.js
+++ public/js/castleHeader.js
@@ -22,7 +22,7 @@ function castleHeader_starsInit()
 
 	stars.maxW 		= displayInfo.w;
 	stars.maxH 		= 440;
-	stars.totalFill = 180;
+	stars.totalFill = 100;
 
 	stars.html0 = '<div class="castleHeader-star0"><div></div></div>';
 	stars.html1 = '<div class="castleHeader-star1"><div></div></div>';
@@ -30,7 +30,8 @@ function castleHeader_starsInit()
 	stars.layer0 = document.querySelector(".castleHeader-wrapper .castleHeader-stars0"); // TINY STARS
 	stars.layer1 = document.querySelector(".castleHeader-wrapper .castleHeader-stars1"); // NORMAL STARS
 
-	stars.props0 = new Array();
+	stars.list0 = new Array();
+	stars.list1 = new Array();
 
 	castleHeader_starsBuild();
 }
@@ -49,15 +50,36 @@ function castleHeader_starsBuild()
 		s0.n = i;
 		s0.x = Math.round(Math.random() * ((stars.maxW - 10) - 10) + 10);
 		s0.y = Math.round(Math.random() * (stars.maxH - 10) + 10);
+		s0.d = Math.round(Math.random() * (1000 - 200) + 200);
 		s0.c = 'star0_' + s0.n;
-		s0.h = '<div class="castleHeader-star0 ' + s0.c + '" style="transform: translate(' + s0.x + 'px, ' + s0.y + 'px);"><div></div></div>';
+		s0.h = '<div class="castleHeader-star0 starTwinkle ' + s0.c + '" style="transform: translate(' + s0.x + 'px, ' + s0.y + 'px); animation-duration: ' + s0.d + 'ms;"><div></div></div>';
 
 		htmlPopulate0 += s0.h;
+
+		stars.list0.push(s0.c);
 	}
 
 	stars.layer0.innerHTML = htmlPopulate0;
 
 	// LARGE STARS
+
+	for(var j = 0; j < stars.totalFill; j++)
+	{
+		var s1 = {};
+		
+		s1.n = j;
+		s1.x = Math.round(Math.random() * ((stars.maxW - 10) - 10) + 10);
+		s1.y = Math.round(Math.random() * (stars.maxH - 10) + 10);
+		s1.d = Math.round(Math.random() * (1000 - 200) + 200);
+		s1.c = 'star1_' + s1.n;
+		s1.h = '<div class="castleHeader-star1 starTwinkle ' + s1.c + '" style="transform: translate(' + s1.x + 'px, ' + s1.y + 'px); animation-duration: ' + s1.d + 'ms;"><div></div></div>';
+
+		htmlPopulate1 += s1.h;
+
+		stars.list0.push(s1.c);
+	}
+
+	stars.layer1.innerHTML = htmlPopulate1;
 }
 
 
